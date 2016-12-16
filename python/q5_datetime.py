d7110b7166d9e6942ca1b662679c74876a063aab
bauer1331 <bauer1331@gmail.com>
2016-12-15 21:14:27 -0500

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,23 @@
 # Hint:  use Google to find python function
 
+from datetime import datetime
 ####a) 
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
+dstart = datetime.strptime(date_start, "%m-%d-%Y")
+dstop = datetime.strptime(date_stop, "%m-%d-%Y")
+dstop - dstart
 
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
+dstart = datetime.strptime(date_start, "%m%d%Y")
+dstop = datetime.strptime(date_stop, "%m%d%Y")
+dstop - dstart
 
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+dstart = datetime.strptime(date_start, "%d-%b-%Y")
+dstop = datetime.strptime(date_stop, "%d-%b-%Y")
+dstop - dstart
